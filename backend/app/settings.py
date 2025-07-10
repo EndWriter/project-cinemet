d047a013f83954a5c1d17b11cc76e6aa67ef90ab
EndWriter <endle.amrane@gmail.com>
2025-07-10 13:51:18 +0200

    setup migration

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cinemet_db',  
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',  
+        'PORT': '5432',        
     }
 }
 
@@ -120,3 +124,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
