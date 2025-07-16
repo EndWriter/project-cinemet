4d754269c38131ed0089447dc8fb1c5fd348f7cf
EndWriter <endle.amrane@gmail.com>
2025-07-16 17:53:35 +0200

    page contact and send mail

--- backend/app/core/views/contact.py
+++ backend/app/core/views/contact.py
@@ -4,8 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.core.mail import send_mail
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def send_contact_email(request):
